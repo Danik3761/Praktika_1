3044448aa5369a2a916862366aea79e503e9ee38
Danik3761 <kowalencko.daniil2016@yandex.ru>
2021-03-15 12:16:07 +0300

    First commit

---
+++ Praktika 1/Adress.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktika_1
+{
+    class Adress
+    {
+        public string Gorod { get; set; }
+        public string Ulica { get; set; }
+        public int NomDoma { get; set; }
+        public Adress()
+        {
+            
+        }
+        public Adress(string gorod, string ulica, int nomDoma)
+        {
+            Gorod = gorod;
+            Ulica = ulica;
+            NomDoma = nomDoma;
+        }
+        public void setGorod(string gorod)
+        {
+            Gorod = gorod;
+        }
+        public string getGorod()
+        {
+            return Gorod;
+        }
+        public void setUlica(string ulica)
+        {
+            Ulica = ulica;
+        }
+        public string getUlica()
+        {
+            return Ulica;
+        }
+        public void setNomDoma(string nomDoma)
+        {
+            NomDoma = Convert.ToInt32(nomDoma);
+        }
+        public int getNomDoma()
+        {
+            return NomDoma;
+        }
+        public void ToString()
+        {
+            Console.Clear();
+            Console.WriteLine("Город: " + Gorod + ", Улица: " + Ulica + ", Номер дома: " + NomDoma);
+        }
+
+    }
+}
---
+++ Praktika 1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktika_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Adress DefaultAdress = new Adress();
+            Console.WriteLine("Приветсвутю, Это практика номер 1");
+            Console.WriteLine("Введите город");
+            string gorod = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Введите улицу");
+            string ulica = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Введите НомерДома");
+            string NomDoma = Convert.ToString(Console.ReadLine());
+            Console.Clear();
+            DefaultAdress.ToString();
+            DefaultAdress.setGorod(gorod);
+            DefaultAdress.setUlica(ulica);
+            DefaultAdress.setNomDoma(NomDoma);
+            DefaultAdress.ToString();
+            Console.ReadKey();
+        }
+    }
+}
